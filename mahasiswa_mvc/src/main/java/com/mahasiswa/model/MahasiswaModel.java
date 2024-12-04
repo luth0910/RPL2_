1830011b01ee4cfe6807a5091090652a5db9dc87
Muhammad Ruhunul Luthfi <98693771+luth0910@users.noreply.github.com>
2024-12-04 19:49:38 +0700

    mhs_mvc

---
+++ mahasiswa_mvc/src/main/java/com/mahasiswa/model/MahasiswaModel.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mahasiswa.model;
+
+/**
+ *
+ * @author mruhu
+ */
+public class MahasiswaModel {
+     private int id;
+    private String nim;
+    private String nama;
+    private int semester;
+    private float ipk;
+    
+    
+    public MahasiswaModel(int id, String npm, String nama, int semester, float ipk) {
+        this.id = id;
+        this.nim = npm;
+        this.nama = nama;
+        this.semester = semester;
+        this.ipk = ipk;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public float getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(float ipk) {
+        this.ipk = ipk;
+    }
+}
